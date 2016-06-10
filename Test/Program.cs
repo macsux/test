8bff98e1719216b005c272498841004a5909939c
Andrew Stakhov <andrew.stakhov@incryptex.com>
2016-06-10 15:19:52 -0400

    Initial

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
